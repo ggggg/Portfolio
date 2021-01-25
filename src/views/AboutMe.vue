95f5bfb34607ea24cf151071719edf34f3f77e64
Ido <benhaim.ido@gmail.com>
2021-01-24 21:56:47 -0500

    added github link

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -3,6 +3,8 @@
     <h1>About Me</h1>
     <p>Hello there! My name is Ido Ben Haim, I am currently a student at King City Secondary School.
       I have a deep passion for computers and computer science.
+    <br>
+    <a href="https://github.com/ggggg" target="_blank">My github</a>
     </p>
     <h4>Technologies I Use:</h4>
     <ul>
